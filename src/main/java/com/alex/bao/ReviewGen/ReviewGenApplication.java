98a3f5ba0dd344abf8dcce46216a6b3c62c97963
baojiawei <baojiawei@lvmama.com>
2017-12-12 14:20:40 +0800

    add code

---
+++ src/main/java/com/alex/bao/ReviewGen/ReviewGenApplication.java
@@ -0,0 +1,12 @@
+package com.alex.bao.ReviewGen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewGenApplication.class, args);
+	}
+}
---
+++ src/test/java/com/alex/bao/ReviewGen/ReviewGenApplicationTests.java
@@ -0,0 +1,16 @@
+package com.alex.bao.ReviewGen;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReviewGenApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
